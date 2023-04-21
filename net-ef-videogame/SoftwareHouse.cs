2937d1250a830216cd3ea034e75efa4805c9a2df
Hadson Roque <nineonezero.light@gmail.com>
2023-04-21 16:34:37 +0200

    Relation one to many

--- net-ef-videogame/SoftwareHouse.cs
+++ net-ef-videogame/SoftwareHouse.cs
@@ -1,12 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace net_ef_videogame
 {
+    [Table("Software_house")]
     internal class SoftwareHouse
     {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Tax_id { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; } 
+        public string? Created_at { get; set; }  
+        public string? Update_at { get; set; }
+
+        public List<Videogame> Videogames { get; set; }
+
+        public SoftwareHouse(long id, string name, string tax_id, string city, string country, string created_at, string update_at)
+        {
+            Id = id;
+            Name = name;
+            Tax_id = tax_id;
+            City = city;
+            Country = country;
+            Created_at = created_at;
+            Update_at = update_at;
+        }
     }
 }
--- net-ef-videogame/Videogame.cs
+++ net-ef-videogame/Videogame.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace net_ef_videogame
 {
+    [Table("Videogames")]
     internal class Videogame
     {
+        public long Id { get; set; }    
+        public string Name { get; set; }
+        public string Overview { get; set; }
+        public DateTime Release_date { get; set; }
+        public DateTime? Created_at { get; set; }
+        public DateTime? Updated_at { get; set; }
+        public long Software_house_id { get; set; }
+        public SoftwareHouse SoftwareHouse { get; set; }
+
+        public Videogame(long id, string name, string overview, DateTime release_date, DateTime created_at, DateTime updated_at, long software_house_id)
+        {
+            Id = id;
+            Name = name;
+            Overview = overview;
+            Release_date = release_date;
+            Created_at = created_at;
+            Updated_at = updated_at;
+            Software_house_id = software_house_id;
+        }
     }
 }
